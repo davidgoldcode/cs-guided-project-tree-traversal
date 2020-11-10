ef7a261e9d763c0b691f8395cc9e70d332673dde
David Gold <davidgold@hey.com>
2020-11-10 18:43:20 -0500

    .

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -15,12 +15,35 @@ Input:
 Output: [3,5,1]
 """
 # Definition for a binary tree node.
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+
 def inorder_traversal(root):
+    pass
     # Your code here
 
+
+def iter_depth_first_traverse(root):
+    if root is None:
+        return []
+
+    result = []
+    stack = []
+    stack.append(root)
+
+    while len(stack) != 0:
+        node = stack.pop()
+        result.append(node.val)
+
+        if node.right is not None:
+            stack.append(node.right)
+        if node.left is not None:
+            stack.append(node.left)
+
+    return result
